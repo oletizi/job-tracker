2ef035421ed69e9b161d93487be07b358def8557
Orion Letizi <oletizi@mac.com>
2018-09-19 17:23:33 -0700

    initial commit

---
+++ src/main/java/com/orionletizi/App.java
@@ -0,0 +1,13 @@
+package com.orionletizi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
